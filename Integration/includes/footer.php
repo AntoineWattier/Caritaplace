1ae2086a5a98bfe00cbda6a446274caeae8d6446
Théo Pipaud <ac-theo-dc@hotmail.fr>
2013-11-14 18:01:35 +0100

    ajout du front

---
+++ Integration/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<p>@ 2013 Groupe 10 - All right reserved</p>
+</footer>
+</body>
+</html>
---
+++ Integration/includes/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8" />
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>Projet HTML/CSS</title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="css/fonts.css"> 
+	  <link rel="stylesheet" href="css/bootstrap.css">
+	  <link rel="stylesheet" href="css/jquery-ui.css">
+	  <link rel="stylesheet" href="rangeSlider/css/ionrangeSlider.css">
+	  <link rel="stylesheet" href="rangeSlider/css/ionrangeSliderskinSimple.css">
+	  <link rel="stylesheet" href="css/style.css">
+	  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  	  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  	  <script src="rangeSlider/js/ion-rangeSlider/ionrangeSlider.js"></script>
+	  <script src="js/bootstrap.js"></script>
+	  <script src="js/script.js"></script>
+	</head>
+	<body>
+		<header>
+			<span></span>
+			<div class="clearfix">
+				<a href="./"><h1><img src="" alt="logo"/></h1></a>
+
+				<div id="panier">
+					<div>
+						<p><strong>20€</strong></p>
+						<p><strong>VOS DONS <br /> 3</strong> associations</p>
+						<a href="#">ACCEDER</a>
+					</div>
+				</div>
+				<span></span>
+			</div>
+		</header>
+		
+	
