4354d7bdb74a9f0192c3f104417d43b2900915c6
lateek35 <lateek35@gmail.com>
2013-11-18 17:23:05 +0100

    MAJ JS selon version local Allan

---
+++ Integration/js/jspoo.js
@@ -0,0 +1,26 @@
+localisation.init({
+    carte : 'map',
+    localized : function (pos) {
+        var crd = pos.coords;
+        var myLatlng = new google.maps.LatLng(crd.latitude,crd.longitude);
+        bounds.extend(myLatlng);
+        var imageMarqueur = new google.maps.MarkerImage('images/pinUser1.png', new google.maps.Size(50, 50), new google.maps.Point(0,0), new google.maps.Point(25, 50));        
+            markerUser = new google.maps.Marker({
+            position: myLatlng,
+            title: "Vous",
+            icon: imageMarqueur
+        });
+        markerUser.setMap(map);
+
+        // remplacer par activation du rayon de recherche
+        document.getElementById('rond').on('click', function(){
+            var taille = parseInt(document.forms["test"].elements["taille"].value);
+            localisation.tri.call(this,taille);
+        });
+
+    }
+    ,
+    nonlocalized : function(pos){
+        console.warn('Impossible d\'effectuer la géolocalisation');
+    }
+});
